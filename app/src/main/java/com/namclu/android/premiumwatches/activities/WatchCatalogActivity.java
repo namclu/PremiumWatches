625524d35fd75a52957a467ab250439e27a4ced2
namclu <namlu1205@gmail.com>
2017-06-04 17:06:53 +1000

    1: add DetailEditorActivity and launch from WatchCatalogActivity

--- app/src/main/java/com/namclu/android/premiumwatches/activities/WatchCatalogActivity.java
+++ app/src/main/java/com/namclu/android/premiumwatches/activities/WatchCatalogActivity.java
@@ -1,13 +1,15 @@
-package com.namclu.android.premiumwatches;
+package com.namclu.android.premiumwatches.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.namclu.android.premiumwatches.R;
 
 public class WatchCatalogActivity extends AppCompatActivity {
 
@@ -22,8 +24,8 @@ public class WatchCatalogActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(WatchCatalogActivity.this, DetailEditorActivity.class);
+                startActivity(intent);
             }
         });
     }
