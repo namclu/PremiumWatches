c7dec6f1b06c278ab5217fd1e220b09c6a0b2312
namclu <namlu1205@gmail.com>
2017-06-04 17:37:13 +1000

    1: add onCreateOptionsMenu() and onOptionsItemSelected()
    - add to DetailEditorActivity

--- app/src/main/java/com/namclu/android/premiumwatches/activities/DetailEditorActivity.java
+++ app/src/main/java/com/namclu/android/premiumwatches/activities/DetailEditorActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.namclu.android.premiumwatches.R;
@@ -29,4 +31,24 @@ public class DetailEditorActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_order_product:
+                return true;
+            case R.id.action_delete_product:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/namclu/android/premiumwatches/activities/WatchCatalogActivity.java
+++ app/src/main/java/com/namclu/android/premiumwatches/activities/WatchCatalogActivity.java
@@ -42,11 +42,11 @@ public class WatchCatalogActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_add_dummy_data:
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
