cd2044bf1f8caf6089de6943eaaec16577bb3b2c
namclu <namlu1205@gmail.com>
2017-06-04 18:06:40 +1000

    2: add WatchContract
    - include inner class WatchEntry that defines
    table_name, _id, columns for watch: model, quantity,
    price, image, supplier, email

---
+++ app/src/main/java/com/namclu/android/premiumwatches/data/WatchContract.java
@@ -0,0 +1,45 @@
+package com.namclu.android.premiumwatches.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by namlu on 6/4/2017.
+ *
+ * Database schema to track an inventory of watches
+ */
+
+public final class WatchContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private WatchContract() {
+
+    }
+
+    /* Inner class that defines the table contents */
+    public static abstract class WatchEntry implements BaseColumns {
+        // Name of the db table for watches
+        public static final String TABLE_NAME = "watches";
+
+        // Unique ID for a watch, Type: INTEGER
+        public static final String _ID = BaseColumns._ID;
+
+        // Name of the watch model, Type: STRING
+        public static final String COLUMN_WATCH_MODEL = "model";
+
+        // Quantity available, Type: INTEGER
+        public static final String COLUMN_WATCH_QUANTITY = "quantity";
+
+        // Price of the watch, Type: INTEGER
+        public static final String COLUMN_WATCH_PRICE = "price";
+
+        // Image of the watch, Type: BLOB
+        public static final String COLUMN_WATCH_IMAGE = "image";
+
+        // Supplier name, Type: STRING
+        public static final String COLUMN_SUPPLIER_NAME = "supplier";
+
+        // Supplier email, Type: STRING
+        public static final String COLUMN_SUPPLIER_EMAIL = "email";
+    }
+}
