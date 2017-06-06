34ef5843ccbb8f84f7a568cabbcce9ac57310031
namclu <namlu1205@gmail.com>
2017-06-06 12:13:01 +1000

    2: add saveWatch() to DetailEditorActivity
    - add Save menu icon to DetailEditorActivity

--- app/src/main/java/com/namclu/android/premiumwatches/activities/DetailEditorActivity.java
+++ app/src/main/java/com/namclu/android/premiumwatches/activities/DetailEditorActivity.java
@@ -1,17 +1,36 @@
 package com.namclu.android.premiumwatches.activities;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.namclu.android.premiumwatches.R;
+import com.namclu.android.premiumwatches.data.WatchContract.WatchEntry;
+import com.namclu.android.premiumwatches.data.WatchDbHelper;
 
+/**
+ * Allows user to create a new watch or edit an existing one.
+ */
 public class DetailEditorActivity extends AppCompatActivity {
+
+    // Declare variables
+    private EditText mModelField;
+    private EditText mPriceField;
+    private EditText mQuantityField;
+    private EditText mSupplierField;
+    private EditText mEmailField;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,14 @@ public class DetailEditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail_editor);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Find views to read user input from
+        mModelField = (EditText) findViewById(R.id.edit_editor_field_model);
+        mPriceField = (EditText) findViewById(R.id.edit_editor_field_price);
+        mQuantityField = (EditText) findViewById(R.id.edit_editor_field_quantity);
+        mSupplierField = (EditText) findViewById(R.id.edit_editor_field_supplier);
+        mEmailField = (EditText) findViewById(R.id.edit_editor_field_email);
+        mImageView = (ImageView) findViewById(R.id.image_editor_product_image);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +70,10 @@ public class DetailEditorActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
+            case R.id.action_save_product:
+                saveWatch();
+                finish();
+                return true;
             case R.id.action_order_product:
                 return true;
             case R.id.action_delete_product:
@@ -50,5 +81,48 @@ public class DetailEditorActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+    * Get user input of Watch from detail editor and saves new Watch into database
+    * */
+    private void saveWatch() {
+        WatchDbHelper dbHelper = new WatchDbHelper(this);
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        // Get String values from details editor field
+        String watchModel = mModelField.getText().toString().trim();
+        String watchPrice = mPriceField.getText().toString().trim();
+        String watchQuantity = mQuantityField.getText().toString().trim();
+        String supplierName = mSupplierField.getText().toString().trim();
+        String supplierEmail = mEmailField.getText().toString().trim();
+
+        ContentValues values = new ContentValues();
+
+        if (!TextUtils.isEmpty(watchModel)) {
+            values.put(WatchEntry.COLUMN_WATCH_MODEL, watchModel);
+        }
+        if (!TextUtils.isEmpty(watchPrice)) {
+            values.put(WatchEntry.COLUMN_WATCH_PRICE, watchPrice);
+        }
+        if (!TextUtils.isEmpty(watchQuantity)) {
+            values.put(WatchEntry.COLUMN_WATCH_QUANTITY, watchQuantity);
+        }
+        if (!TextUtils.isEmpty(supplierName)) {
+            values.put(WatchEntry.COLUMN_SUPPLIER_NAME, supplierName);
+        }
+        if (!TextUtils.isEmpty(supplierEmail)) {
+            values.put(WatchEntry.COLUMN_SUPPLIER_EMAIL, supplierEmail);
+        }
+
+        // Insert Watch into db.
+        // Returns row ID of the newly inserted row, or -1 if an error occurred
+        long rowId = database.insert(WatchEntry.TABLE_NAME, null, values);
+
+        if (rowId == -1) {
+            Toast.makeText(this, "Error saving item", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "New item added at row " + rowId, Toast.LENGTH_SHORT).show();
+        }
     }
 }
